f1d2f76d0db87a1ad51640d75fd8a748b1501df4
Ives Samuel <48871044+Samuelves@users.noreply.github.com>
2021-03-24 22:56:53 -0300

    Update movieapi.ts

--- services/movieapi.ts
+++ services/movieapi.ts
@@ -10,56 +10,56 @@ export const HomeList = async (): Promise<T> => {
       slug: 'originals',
       title: 'Originais do Netflix',
       items: await basicFecth(
-        `/discover/tv/?with_network=213&language=pt-BR&api_key=${process.env.API_KEY}`
+        `/discover/tv/?with_network=213&language=pt-BR&api_key=${process.env.API_KEY_MOVIE}`
       )
     },
     {
       slug: 'trending',
       title: 'Recomendados para Você',
       items: await basicFecth(
-        `/trending/all/week?language=pt-BR&api_key=${process.env.API_KEY}`
+        `/trending/all/week?language=pt-BR&api_key=${process.env.API_KEYY_MOVIE}`
       )
     },
     {
       slug: 'toprated',
       title: 'Em Alta',
       items: await basicFecth(
-        `/movie/top_rated?&language=pt-BR&api_key=${process.env.API_KEY}`
+        `/movie/top_rated?&language=pt-BR&api_key=${process.env.API_KEYY_MOVIE}`
       )
     },
     {
       slug: 'action',
       title: 'Ação',
       items: await basicFecth(
-        `/discover/movie?with_genres=28&language=pt-BR&api_key=${process.env.API_KEY}`
+        `/discover/movie?with_genres=28&language=pt-BR&api_key=${process.env.API_KEYY_MOVIE}`
       )
     },
     {
       slug: 'comedy',
       title: 'Comédia',
       items: await basicFecth(
-        `/discover/movie?with_genres=35&language=pt-BR&api_key=${process.env.API_KEY}`
+        `/discover/movie?with_genres=35&language=pt-BR&api_key=${process.env.API_KEYY_MOVIE}`
       )
     },
     {
       slug: 'horror',
       title: 'Terror',
       items: await basicFecth(
-        `/discover/movie?with_genres=27&language=pt-BR&api_key=${process.env.API_KEY}`
+        `/discover/movie?with_genres=27&language=pt-BR&api_key=${process.env.API_KEYY_MOVIE}`
       )
     },
     {
       slug: 'romance',
       title: 'Romance',
       items: await basicFecth(
-        `/discover/movie?with_genres=10749&language=pt-BR&api_key=${process.env.API_KEY}`
+        `/discover/movie?with_genres=10749&language=pt-BR&api_key=${process.env.API_KEYY_MOVIE}`
       )
     },
     {
       slug: 'documentary',
       title: 'Documentários',
       items: await basicFecth(
-        `/discover/movie?with_genres=99&language=pt-BR&api_key=${process.env.API_KEY}`
+        `/discover/movie?with_genres=99&language=pt-BR&api_key=${process.env.API_KEYY_MOVIE}`
       )
     }
   ]
@@ -73,10 +73,10 @@ export const getMovieInfo = async (
   try {
     type === 'movie'
       ? (info = await basicFecth(
-          `/movie/${movieId}?language=pt-BR&api_key=${process.env.API_KEY}`
+          `/movie/${movieId}?language=pt-BR&api_key=${process.env.API_KEYY_MOVIE}`
         ))
       : (info = await basicFecth(
-          `/tv/${movieId}?language=pt-BR&api_key=${process.env.API_KEY}`
+          `/tv/${movieId}?language=pt-BR&api_key=${process.env.API_KEYY_MOVIE}`
         ))
   } catch {
     info = null
